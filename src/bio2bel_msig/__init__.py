1bac65a064c60f7956e5b1eb8d3c8f04820dd1a0
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-03-06 09:59:05 +0100

    Remove unnecessary stuffs in init

--- src/bio2bel_msig/__init__.py
+++ src/bio2bel_msig/__init__.py
@@ -8,31 +8,6 @@ Citation
 - Subramanian, A., Tamayo, P., Mootha, V. K., Mukherjee, S., Ebert, B. L., Gillette, M. A., ... & Mesirov, J. P. (2005). Gene set enrichment analysis: a knowledge-based approach for interpreting genome-wide expression profiles. Proceedings of the National Academy of Sciences, 102(43), 15545-15550.
 """
 
-import logging
-
-from pkg_resources import VersionConflict, iter_entry_points
-
-from .constants import MODULE_NAME
-
-log = logging.getLogger(__name__)
-
-managers = {}
-
-for entry_point in iter_entry_points(group=MODULE_NAME, name=None):
-    entry = entry_point.name
-
-    try:
-        bio2bel_module = entry_point.load()
-    except VersionConflict:
-        log.warning('Version conflict in %s', entry)
-        continue
-
-    try:
-        managers[entry] = bio2bel_module.Manager
-    except Exception:
-        log.warning('%s does not have a top-level Manager class', entry)
-        continue
-
 __version__ = '0.0.1-dev'
 
 __title__ = 'bio2bel_msig'
