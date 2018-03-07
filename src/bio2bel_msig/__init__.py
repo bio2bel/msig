9428cb644ab99e4e663d51ceef514ad4cfb791c2
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-03-07 11:03:20 +0100

    Update init file

--- src/bio2bel_msig/__init__.py
+++ src/bio2bel_msig/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 """
-Bio2BEL MSIG is a package for enriching BEL networks with MSIG DB. Furthermore, it is integrated in the `ComPath environment <https://github.com/ComPath>`_ for pathway database comparison.
+Bio2BEL MSig is a package for enriching BEL networks with MsigDB. Furthermore, it is integrated in the `ComPath environment <https://github.com/ComPath>`_ for pathway database comparison.
 
 Citation
 --------
 - Subramanian, A., Tamayo, P., Mootha, V. K., Mukherjee, S., Ebert, B. L., Gillette, M. A., ... & Mesirov, J. P. (2005). Gene set enrichment analysis: a knowledge-based approach for interpreting genome-wide expression profiles. Proceedings of the National Academy of Sciences, 102(43), 15545-15550.
+- Liberzon, A., Subramanian, A., Pinchback, R., Thorvaldsdóttir, H., Tamayo, P., & Mesirov, J. P. (2011). Molecular signatures database (MSigDB) 3.0. Bioinformatics, 27(12), 1739-1740.
+
+
 """
 
 from .manager import Manager
