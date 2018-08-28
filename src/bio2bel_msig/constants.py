3657572f3434042a9868915e3f80809b7f801d19
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-08-28 15:30:51 +0200

    Remove storing of default cache connection

--- src/bio2bel_msig/constants.py
+++ src/bio2bel_msig/constants.py
@@ -4,11 +4,10 @@
 
 import os
 
-from bio2bel.utils import get_connection, get_data_dir
+from bio2bel import get_data_dir
 
 MODULE_NAME = 'msig'
 DATA_DIR = get_data_dir(MODULE_NAME)
-DEFAULT_CACHE_CONNECTION = get_connection(MODULE_NAME)
 
 CONFIG_FILE_PATH = os.path.join(DATA_DIR, 'config.ini')
 
